4b4dcd20f50d42e7300e7a5fa3f90c777d991002
Adrian Gomez Reyes <kunkkaharden@protonmail.com>
2023-01-18 20:48:04 -0500

    add db

---
+++ db/config.ts
@@ -0,0 +1,12 @@
+import {connect} from "mongoose";
+
+export const dbConection = async () => {
+    const url = `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+  try {
+    await connect(url);
+    console.log("DB ok");
+  } catch (error) {
+    console.log(error);
+    throw new Error("Internal Server Error");
+  }
+};
--- index.ts
+++ index.ts
@@ -2,11 +2,12 @@ import express , {  Request, Response } from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import authRoutes from './routes/auth';
-
+import { dbConection } from './db/config';
 dotenv.config();
 
 const PORT = process.env.PORT;
 const app = express();
+dbConection();
 
 app.use(cors());
 app.use(express.json());
